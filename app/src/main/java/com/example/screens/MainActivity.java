0ac1e8de25469ecec31c94014aa3d939490fb154
Abdalrahman Alhindi <71189461+Samurai-Ebben@users.noreply.github.com>
2024-04-18 21:51:34 +0200

    SecondActivity Takes in extra intent

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("Name", "Abdalrahman Alhindi");
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -1,8 +1,11 @@
 package com.example.screens;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        TextView msg = (TextView) findViewById(R.id.text);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String name = extras.getString("Name");
+            msg.setText("Hello: " + name);
+        }
     }
 }
